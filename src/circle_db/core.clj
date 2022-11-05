92a2425d4a6dc9ba8659ea2ec8c89880da0f898a
sernamar <sernamar@protonmail.com>
2022-11-05 09:39:40 +0100

    Store the data in memory for now

--- src/circle_db/core.clj
+++ src/circle_db/core.clj
@@ -20,3 +20,18 @@
 (defn add-attribute [entity attribute]
    (let [attribute-id (keyword (:name attribute))]
       (assoc-in entity [:attributes attribute-id] attribute)))
+
+;;; ------- ;;;
+;;; Storage ;;;
+;;; ------- ;;;
+
+(defprotocol Storage
+  (get-entity [storage entity-id])
+  (write-entity [storage entity])
+  (drop-entity [storage entity]))
+
+(defrecord InMemory []
+  Storage
+  (get-entity [storage entity-id] (entity-id storage))
+  (write-entity [storage entity] (assoc storage (:id entity) entity))
+  (drop-entity [storage entity] (dissoc storage (:id entity))))
